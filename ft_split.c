73f4fdcd25c542f64474cfd693bfba3297607a34
subaru3rem <cainancosta6@gmail.com>
2024-10-15 20:54:08 -0300

    split vai estar dando bom, confio

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: ccosta <ccosta@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 22:19:36 by ccosta            #+#    #+#             */
-/*   Updated: 2024/10/02 01:34:51 by ccosta           ###   ########.fr       */
+/*   Updated: 2024/10/15 20:53:32 by ccosta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,43 +14,71 @@
 
 static	int	split_len(char const *str, char src)
 {
+	int co;
 	int	count;
 
-	count = 1;
-	while (*str)
+	count = 0;
+	co = -1;
+	while (str[++co])
 	{
-		if (*str == src)
+		if (str[co] != src)
+			continue;
+		if (str[co] == src && (str[co + 1] == src || str[co + 1] == 0))
+			continue;
+		else
+		{
 			count++;
-		str++;
+		}
 	}
+	if (count == 0)
+		count++;
 	return (count);
+}
+
+static char *ft_getstr(char const *str, char c)
+{
+	int		interval;
+	char	*pointer;
+	char	*final;
+
+	interval = (ft_strchr((char *)str, c) + 1) - str;
+	if (interval < 0)
+		interval = ft_strlen(str);
+	pointer = malloc(interval);
+	ft_strlcpy(pointer, str, interval);
+	final = ft_strdup(pointer);
+	free(pointer);
+	return (final);
 }
 
 char	**ft_split(char const *s, char c)
 {
 	int		co;
-	int		count;
 	int		init;
-	char	*pointer;
 	char	**strs;
 
-	count = 0;
-	init = 0;
 	co = 0;
-	pointer = malloc(sizeof(char) * (ft_strlen(s) + 1));
+	init = 0;
 	strs = malloc(sizeof(char *) * (split_len(s, c) + 1));
-	strs[init++] = pointer;
-	while (s[count])
+	if (s[0] == '\0')
 	{
-		if (s[count++] != c)
-			pointer[co++] = s[count - 1];
+		strs[0] = NULL;
+		return (strs);
+	}
+	if (s[co] != c)
+		strs[init++] = ft_getstr(s, c);
+	co--;
+	while (s[++co])
+	{
+		if (s[co] != c)
+			continue;
+		if (s[co] == c && (s[co + 1] == c || s[co + 1] == 0))
+			continue;
 		else
 		{
-			pointer[co++] = '\0';
-			strs[init++] = &pointer[co];
+			strs[init++] = ft_getstr(&s[co+1], c);
 		}
 	}
-	pointer[co] = '\0';
 	strs[init] = NULL;
 	return (strs);
 }
